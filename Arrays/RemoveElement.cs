3390299bab9858aa95e2f57120d79d3e5117cd5e
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-09-12 07:32:30 +0545

    First Commit

---
+++ Arrays/RemoveElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.LeetCode_Challenges.Arrays
+{
+  public  class RemoveElement
+    {
+        public int RemoveElementAt(int[] nums,int val)
+        {
+            int contains=0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                    contains++;
+            }
+            return nums.Length - contains;
+
+
+        }
+    }
+}
